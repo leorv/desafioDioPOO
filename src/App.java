57e1cd5f4388f2b70eac742816d74a138ab5df7b
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-07-27 19:57:41 -0300

    abstração e encapsulamento.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,27 @@
+import java.time.LocalDate;
+
+import dominio.Curso;
+import dominio.Mentoria;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Angular");
+        curso2.setDescricao("Descrição do curso Angular");
+        curso2.setCargaHoraria(32);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
