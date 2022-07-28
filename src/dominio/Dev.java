d6360e9a3048811f5b3b4e0665dcae1a8e373cc1
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-07-27 21:29:00 -0300

    herança e polimorfismo pt2

---
+++ src/dominio/Dev.java
@@ -0,0 +1,43 @@
+package dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+
+    }
+
+    public void progredir() {
+
+    }
+
+    public void calcularTotalXP() {
+
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Dev)) {
+            return false;
+        }
+        Dev dev = (Dev) o;
+        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+    }
+
+
+    
+}
