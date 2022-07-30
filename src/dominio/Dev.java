092b771bd6b3a13d24ffa473b9b07b375bf45e68
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-07-30 18:01:44 -0300

    concluindo as classes de dominio

--- src/dominio/Dev.java
+++ src/dominio/Dev.java
@@ -2,6 +2,7 @@ package dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -10,15 +11,26 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()){
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo.");
+        }
     }
 
-    public void calcularTotalXP() {
-
+    public double calcularTotalXP() {
+        return this.conteudosConcluidos
+            .stream()
+            .mapToDouble(conteudo -> conteudo.calcularXP())
+            // .mapToDouble(conteudo::conteudo.calcularXP()) <= também funciona.
+            .sum();
     }
 
 
